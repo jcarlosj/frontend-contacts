2f063f9cda76169e5f8478f18005de31c4cdc601
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-06-14 12:44:03 -0500

    [+] app.module.ts: imports > ReactiveFormsModule & providers > provideHttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { PageContactComponent } from './pages/page-contact/page-contact.component';
 import { ContactFormComponent } from './components/contact/contact-form/contact-form.component';
 import { ContactListComponent } from './components/contact/contact-list/contact-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,14 @@ import { ContactListComponent } from './components/contact/contact-list/contact-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(
+      withFetch(),
+    )
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
