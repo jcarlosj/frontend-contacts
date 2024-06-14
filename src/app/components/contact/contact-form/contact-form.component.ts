59e5c33f6c94807a4afe8c609b3018a448aaa9a9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-06-14 12:53:12 -0500

    [+] contact-form.component.*: Define formulario, validaciones, mensajes de error, Submit, implementa el Servicio

--- src/app/components/contact/contact-form/contact-form.component.ts
+++ src/app/components/contact/contact-form/contact-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  registroForm!: FormGroup; // Crear formulario reactivo
 
+  constructor(private dataService: DataService) { } // Inyectar servicio de datos
+
+  ngOnInit(): void {
+    this.registroForm = new FormGroup({
+      nombre: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+  }
+
+  onSubmit(): void {
+    const datos = this.registroForm.value; // Obtener valores del formulario
+    this.dataService.guardarRegistro(datos); // Guardar datos usando el servicio de datos
+    this.registroForm.reset(); // Limpiar el formulario
+  }
 }
+
