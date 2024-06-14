18ea6c996fa7c6adddd8a648a3281391b5a4eddc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-06-14 12:25:27 -0500

    [+] ng g c (page-component, contact-form, contact-list) [+] app-routing.module.ts: Define rutas disponibles
    Componentes creados
      ng g c pages/page-contact
      ng g c components/contact/contact-form
      ng g c components/contact/contact-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageContactComponent } from './pages/page-contact/page-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact', component: PageContactComponent },
+  { path: '', redirectTo: 'contact', pathMatch: 'full' },
+  { path: '**', redirectTo: 'contact', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageContactComponent } from './pages/page-contact/page-contact.component';
+import { ContactFormComponent } from './components/contact/contact-form/contact-form.component';
+import { ContactListComponent } from './components/contact/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageContactComponent,
+    ContactFormComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
